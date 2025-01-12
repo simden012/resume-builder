0672f712462e68fef7a8172336bea93e621b6eea
Simon Denis <simon.denis@polymtl.ca>
2025-01-12 13:11:59 -0500

    exported the interface into their own file

---
+++ client/src/interfaces/types.ts
@@ -0,0 +1,15 @@
+export interface Project {
+  name: string;
+  description: string;
+}
+export interface WorkExperience {
+  jobTitle: string;
+  company: string;
+  duration: string;
+  description: string;
+}
+export interface Education {
+  degree: string;
+  institution: string;
+  year: string;
+}
